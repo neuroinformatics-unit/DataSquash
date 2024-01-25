9184f10ee5ba1ef49ecbfdfbf4b65d46bff47e8c
sfmig <33267254+sfmig@users.noreply.github.com>
2024-01-25 18:05:40 +0000

    draft bash scripts

--- datasquash/video_compression/bash_scripts/reencode_videos.sh
+++ datasquash/video_compression/bash_scripts/reencode_videos.sh
@@ -1,61 +1,30 @@
 #!/bin/bash
 
-#SBATCH -p gpu # partitions: cpu, gpu, fast, medium
+#SBATCH -p gpu # partition
 #SBATCH -N 1   # number of nodes
 #SBATCH --mem 64G # memory pool for all cores
 #SBATCH -n 2 # number of cores
 #SBATCH -t 3-00:00 # time (D-HH:MM)
-#SBATCH --gres gpu:0 # request NO GPUs
+#SBATCH --gres gpu:1 # request 1 GPU (of any kind)
 #SBATCH -o slurm_array.%N.%A-%a.out
 #SBATCH -e slurm_array.%N.%A-%a.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=s.minano@ucl.ac.uk
-#SBATCH --array=0-2%4
+#SBATCH --array=0-5%5
 
 
 # NOTE on SBATCH command for array jobs
 # with "SBATCH --array=0-n%m" ---> runs n separate jobs, but not more than m at a time.
 # the number of array jobs should match the number of input files
 
-# Refs:
-# - https://sleap.ai/help.html#does-my-data-need-to-be-in-a-particular-format
-
-
-# -----------------------------
-# Error settings for bash
-# -----------------------------
-# see https://wizardzines.com/comics/bash-errors/
-set -e  # do not continue after errors
-set -u  # throw error if variable is unset
-set -o pipefail  # make the pipe fail if any part of it fails
-
-
 # ----------------------
 # Input & output data
 # ----------------------
 PROJ_DIR=/ceph/neuroinformatics/neuroinformatics/sminano/video-compression/
+INPUT_VIDEO=datasets/drosophila-melanogaster-courtship/20190128_113421.mp4
+OUTPUT_SUBDIR=reencoded-videos
 
-# input videos
-INPUT_VIDEO=$PROJ_DIR/input-videos/20190128_113421.mp4
-
-# output directory (location of reencoded videos)
-REENCODED_VIDEOS_DIR=$PROJ_DIR/input-videos
-mkdir -p $REENCODED_VIDEOS_DIR # create if it doesnt exist
-
-# logs directory
-LOG_DIR=$REENCODED_VIDEOS_DIR/logs
-mkdir -p $LOG_DIR  # create if it doesnt exist
-
-
-# ----------------------
-# Encoding parameters
-# ----------------------
-CRF_VALUES=($(seq 17 17 51))
-
-
-# ----------------------
-# Input data checks
-# ----------------------
+CRF_VALUES=($(seq 5 5 25))
 # Check len(list of input crf values) matches max SLURM_ARRAY_TASK_COUNT
 # if not, exit
 if [[ $SLURM_ARRAY_TASK_COUNT -ne ${#CRF_VALUES[@]} ]]; then
@@ -64,61 +33,60 @@ if [[ $SLURM_ARRAY_TASK_COUNT -ne ${#CRF_VALUES[@]} ]]; then
 fi
 
 
-# ----------------------------------------------------
-# Compress input video with different values of crf
-# ----------------------------------------------------
+
+# location of SLURM logs
+LOG_DIR=$PROJ_DIR/logs
+mkdir -p $LOG_DIR  # create if it doesnt exist
+
+# set location of reencoded videos
+REENCODED_VIDEOS_DIR=$PROJ_DIR/$OUTPUT_SUBDIR
+mkdir -p $REENCODED_VIDEOS_DIR # create if it doesnt exist
+
+
+# ----------------------------------------
+# Compress with different values of crf
+# --------------------------------------------
 for i in {1..${SLURM_ARRAY_TASK_COUNT}}
 do
     # Input video
-    echo "Input video: $INPUT_VIDEO"
+    SAMPLE=$INPUT_VIDEO
+    echo "Input video: $SAMPLE"
     echo "--------"
+
+    # Reencode video following SLEAP's recommendations
+    # https://sleap.ai/help.html#does-my-data-need-to-be-in-a-particular-format
+    echo "Rencoding ...."
 
     # Path to reencoded video
-    filename_no_ext="$(basename "$INPUT_VIDEO" | sed 's/\(.*\)\..*/\1/')" # filename without extension
-    filename_out_no_ext="$filename_no_ext"_CRF"${CRF_VALUES[${SLURM_ARRAY_TASK_ID}]}"
-    REENCODED_VIDEO_PATH_MP4="$REENCODED_VIDEOS_DIR/$filename_out_no_ext".mp4  # must be .mp4
+    filename_no_ext="$(basename "$SAMPLE" | sed 's/\(.*\)\..*/\1/')" # filename without extension
+    REENCODED_VIDEO_PATH="$REENCODED_VIDEOS_DIR/$filename_no_ext"_RE.$reencoded_extension
 
-    # Print current node
-    echo "SLURM node: $SLURMD_NODENAME"
-    echo "--------"
-
-    # Print ffmpeg version to logs
-    ffmpeg -version
-
-    # Print ffprobe to logs
-    # TODO: check/extract properties of initial video (8-bit vs 10-bit, rgb vs greyscale)
-    ffprobe -v error -show_streams $INPUT_VIDEO
-
-    # Run compression with the corresponding CRF value
-    # - y: overwrite output files without asking.
-    # - c:v: specify codec for the video stream
-    #   (decoder if used in front of input, encoder if used in front of output).
-    # - libx264: sets the video compression to use H264. If RGB, use libx264rgb
-    # - preset superfast: sets a number of parameters that pressumably enable reliable seeking
-    # - c:a # keep audio stream as is if present
-    ffmpeg -y -i "$INPUT_VIDEO" \
+    # Run ffmpeg with the corresponding crf value
+    ffmpeg -version  # print version to logs
+    ffmpeg -y -i "$SAMPLE" \
     -c:v libx264 \
     -pix_fmt yuv420p \
     -preset superfast \
-    -crf "${CRF_VALUES[${SLURM_ARRAY_TASK_ID}]}" \
-    -c:a copy \
-    "$REENCODED_VIDEO_PATH_MP4"
-
-    # collect status of previous command
-    status_ffmpeg_compress=$?
+    -crf "${CRF_VALUES[${SLURM_ARRAY_TASK_ID}]}" \  #15 \
+    $REENCODED_VIDEO_PATH
 
 
-    # print only if previous command is successful
-    if [[ "$status_ffmpeg_compress" -eq 0 ]] ; then
-        echo "Reencoded video: $REENCODED_VIDEO_PATH_MP4"
-        echo "--------"
-    else
-        echo "ERROR reencoding video: $REENCODED_VIDEO_PATH_MP4"
-        echo "--------"
-    fi
+    echo "Reencoded video: $REENCODED_VIDEO_PATH"
+    echo "--------"
 
-    # Move logs across
-    mv slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.{err,out} \
-    /$LOG_DIR/"$filename_out_no_ext".slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.{err,out}
+
+    # Reencoded videos log
+    # copy .err file to go with reencoded video too if required
+    # filename: {reencoded video name}.{slurm_array}.{slurm_job_id}
+    # TODO: make a nicer log
+    for ext in err out
+    do
+    cp slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.$ext \
+    /$REENCODED_VIDEOS_SUBDIR/"$filename_no_ext"_RE.slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.$ext
+    done
+
+    # Frame extraction logs
+    # Move logs for this job to subdir with extracted frames
+    mv slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.{err,out} /$LOG_DIR
 
 done
--- datasquash/video_compression/bash_scripts/run_training.sh
+++ datasquash/video_compression/bash_scripts/run_training.sh
@@ -1,142 +1,37 @@
 #!/bin/bash
-
-#SBATCH -p gpu # partition
-#SBATCH -N 1   # number of nodes
-#SBATCH --mem 64G # memory pool for all cores
-#SBATCH -n 2 # number of cores
-#SBATCH -t 3-00:00 # time (D-HH:MM)
-#SBATCH --gres gpu:rtx5000:1 # request 1 GPU RTX5000
-#SBATCH -o slurm_array.%N.%A-%a.out
-#SBATCH -e slurm_array.%N.%A-%a.err
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=s.minano@ucl.ac.uk
-#SBATCH --array=0-3%4
-
 # Refs:
 # - https://sleap.ai/notebooks/Training_and_inference_on_an_example_dataset.html
 # - https://sleap.ai/notebooks/Model_evaluation.html
 # - https://sleap.ai/guides/cli.html#sleap-train
 
 
-# -----------------------------
-# Error settings for bash
-# -----------------------------
-# see https://wizardzines.com/comics/bash-errors/
-set -e  # do not continue after errors
-set -u  # throw error if variable is unset
-set -o pipefail  # make the pipe fail if any part of it fails
+# Module load sleap
 
 
-# -------------------------------
-# Load most recent sleap module
-# ------------------------------
-module load SLEAP
+# Run training for each reencoded video
+# - for a top-down pipeline, you’ll have a different profile for each of the models:
+#   centroid.json and centered_instance.json,
+# - for a bottom-up pipeline approach: multi_instance.json
 
-# ----------------------
-# Input & output data
-# ----------------------
-PROJ_DIR=/ceph/neuroinformatics/neuroinformatics/sminano/video-compression/
-
-# input videos
-INPUT_VIDEOS_DIR="$PROJ_DIR/input-videos"
-INPUT_VIDEOS_LIST=(
-    "$INPUT_VIDEOS_DIR/20190128_113421.mp4"
-    "$INPUT_VIDEOS_DIR/20190128_113421_CRF17.mp4"
-    "$INPUT_VIDEOS_DIR/20190128_113421_CRF34.mp4"
-    "$INPUT_VIDEOS_DIR/20190128_113421_CRF51.mp4"
-)
-
-# input labels
-SLEAP_LABELS_DIR="$PROJ_DIR/input-labels"
-SLEAP_LABELS_REF_FILE="$PROJ_DIR/datasets/drosophila-melanogaster-courtship/courtship_labels.slp"
-labels_ref_filename_no_ext="$(basename "$SLEAP_LABELS_REF_FILE" | sed 's/\(.*\)\..*/\1/')"
-
-# save logs inside SLEAP model folder
-LOG_DIR=$PROJ_DIR/models/logs
-mkdir -p $LOG_DIR  # create if it doesnt exist
+# bottom up
+sleap-train \
+    multi_instance.json \  #----> ok?
+    "dataset/drosophila-melanogaster-courtship/courtship_labels.slp" \
+    --run_name "courtship.centroid" \
+    --video-paths "dataset/drosophila-melanogaster-courtship/20190128_113421.mp4"
 
 
-# ----------------------
-# Input data checks
-# ----------------------
-# Check len(list of input data) matches max SLURM_ARRAY_TASK_COUNT
-# if not, exit
-if [[ $SLURM_ARRAY_TASK_COUNT -ne ${#INPUT_VIDEOS_LIST[@]} ]]; then
-    echo "The number of array tasks does not match the number of inputs"
-    exit 1
-fi
-
-# TODO: check input labels files match video files
-# labels files assumed to follow the naming convention:
-#  <SLEAP_LABELS_REF_FILE>_<video_filename_no_ext>.slp
+# centroid model
+sleap-train \
+    baseline.centroid.json \
+    "dataset/drosophila-melanogaster-courtship/courtship_labels.slp" \
+    --run_name "courtship.centroid" \
+    --video-paths "dataset/drosophila-melanogaster-courtship/20190128_113421.mp4"
 
 
-# ------------------------------------------------------
-# Train a topdown SLEAP model for each reencoded video
-# ------------------------------------------------------
-
-for i in {1..${SLURM_ARRAY_TASK_COUNT}}
-do
-    # input video
-    INPUT_VIDEO=${INPUT_VIDEOS_LIST[${SLURM_ARRAY_TASK_ID}]}
-    video_filename_no_ext="$(basename "$INPUT_VIDEO" | sed 's/\(.*\)\..*/\1/')"
-    echo "Input video: $INPUT_VIDEO"
-
-    # train centroid model
-    # TODO: --video-paths maybe "$PROJ_DIR/input-videos/ instead?
-    sleap-train \
-        baseline.centroid.json \
-        "$SLEAP_LABELS_DIR/$labels_ref_filename_no_ext"_$video_filename_no_ext.slp \
-        --video-paths "$INPUT_VIDEO" \
-        --run_name $video_filename_no_ext \
-        --suffix "_centroid_model.slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID" \
-        --tensorboard
-
-    # collect status of previous command
-    status_sleap_train_centroid=$?
-
-    # print success to logs
-    if [[ "$status_sleap_train_centroid" -eq 0 ]] ; then
-        echo "Centroid model training complete"
-        echo "---"
-    else
-        echo "ERROR training centroid model"
-        echo "---"
-    fi
-
-
-    # train centred instance model
-    sleap-train \
-        baseline_medium_rf.topdown.json \
-        "$SLEAP_LABELS_DIR/$labels_ref_filename_no_ext"_$video_filename_no_ext.slp \
-        --video-paths "$INPUT_VIDEO" \
-        --run_name $video_filename_no_ext \
-        --suffix "_centered_instance_model.slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID" \
-        --tensorboard
-
-    # collect status of previous command
-    status_sleap_train_centered_instance=$?
-
-    # print success to logs
-    if [[ "$status_sleap_train_centered_instance" -eq 0 ]] ; then
-        echo "Centered instance model training complete"
-        echo "---"
-    else
-        echo "ERROR training centered instance model"
-        echo "---"
-    fi
-
-
-    # move models folder across
-    mv models/ $PROJ_DIR/models
-
-    # move logs across
-    for ext in err out
-        do
-            mv slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.$ext \
-            /$LOG_DIR/$(basename "$video_filename_no_ext").slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.$ext
-        done
-
-
-
-done
+# centred instance model
+sleap-train \
+    baseline_medium_rf.topdown.json \
+    "dataset/drosophila-melanogaster-courtship/courtship_labels.slp" \
+    --run_name "courtship.topdown_confmaps" \
+    --video-paths "dataset/drosophila-melanogaster-courtship/20190128_113421.mp4"
