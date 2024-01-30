fff282c934dc8e53622e7c75968622f572de6e16
sfmig <33267254+sfmig@users.noreply.github.com>
2024-01-30 17:02:13 +0000

    refactored, added last command checks and moving logs (not checked in the cluster)

--- datasquash/video_compression/bash_scripts/reencode_videos.sh
+++ datasquash/video_compression/bash_scripts/reencode_videos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -p gpu # partition: cpu, gpu, fast, medium
+#SBATCH -p gpu # partitions: cpu, gpu, fast, medium
 #SBATCH -N 1   # number of nodes
 #SBATCH --mem 64G # memory pool for all cores
 #SBATCH -n 2 # number of cores
@@ -17,14 +17,45 @@
 # with "SBATCH --array=0-n%m" ---> runs n separate jobs, but not more than m at a time.
 # the number of array jobs should match the number of input files
 
+# Refs:
+# - https://sleap.ai/help.html#does-my-data-need-to-be-in-a-particular-format
+
+
+# -----------------------------
+# Error settings for bash
+# -----------------------------
+# see https://wizardzines.com/comics/bash-errors/
+set -e  # do not continue after errors
+set -u  # throw error if variable is unset
+set -o pipefail  # make the pipe fail if any part of it fails
+
+
 # ----------------------
 # Input & output data
 # ----------------------
 PROJ_DIR=/ceph/neuroinformatics/neuroinformatics/sminano/video-compression/
-INPUT_VIDEO=$PROJ_DIR/input-videos/20190128_113421.mp4
-OUTPUT_SUBDIR=input-videos
 
+# input videos
+INPUT_VIDEO=$PROJ_DIR/input-videos/20190128_113421.mp4
+
+# output directory (location of reencoded videos)
+REENCODED_VIDEOS_DIR=$PROJ_DIR/input-videos
+mkdir -p $REENCODED_VIDEOS_DIR # create if it doesnt exist
+
+# logs directory
+LOG_DIR=$REENCODED_VIDEOS_DIR/logs
+mkdir -p $LOG_DIR  # create if it doesnt exist
+
+
+# ----------------------
+# Encoding parameters
+# ----------------------
 CRF_VALUES=($(seq 17 17 51))
+
+
+# ----------------------
+# Input data checks
+# ----------------------
 # Check len(list of input crf values) matches max SLURM_ARRAY_TASK_COUNT
 # if not, exit
 if [[ $SLURM_ARRAY_TASK_COUNT -ne ${#CRF_VALUES[@]} ]]; then
@@ -33,72 +64,61 @@ if [[ $SLURM_ARRAY_TASK_COUNT -ne ${#CRF_VALUES[@]} ]]; then
 fi
 
 
-# set location of reencoded videos
-REENCODED_VIDEOS_DIR=$PROJ_DIR/$OUTPUT_SUBDIR
-mkdir -p $REENCODED_VIDEOS_DIR # create if it doesnt exist
-
-
-# location of SLURM logs
-LOG_DIR=$REENCODED_VIDEOS_DIR/logs
-mkdir -p $LOG_DIR  # create if it doesnt exist
-
-# ----------------------------------------
-# Compress with different values of crf
-# --------------------------------------------
+# ----------------------------------------------------
+# Compress input video with different values of crf
+# ----------------------------------------------------
 for i in {1..${SLURM_ARRAY_TASK_COUNT}}
 do
     # Input video
-    SAMPLE=$INPUT_VIDEO
-    echo "Input video: $SAMPLE"
+    echo "Input video: $INPUT_VIDEO"
     echo "--------"
 
-    # Reencode video following SLEAP's recommendations
-    # https://sleap.ai/help.html#does-my-data-need-to-be-in-a-particular-format
-    echo "Rencoding ...."
-
     # Path to reencoded video
-    filename_no_ext="$(basename "$SAMPLE" | sed 's/\(.*\)\..*/\1/')" # filename without extension
+    filename_no_ext="$(basename "$INPUT_VIDEO" | sed 's/\(.*\)\..*/\1/')" # filename without extension
     filename_out_no_ext="$filename_no_ext"_CRF"${CRF_VALUES[${SLURM_ARRAY_TASK_ID}]}"
-    REENCODED_VIDEO_PATH_MP4="$REENCODED_VIDEOS_DIR/$filename_out_no_ext".mp4  # must be .mp4?
+    REENCODED_VIDEO_PATH_MP4="$REENCODED_VIDEOS_DIR/$filename_out_no_ext".mp4  # must be .mp4
+
+    # Print current node
+    echo "SLURM node: $SLURMD_NODENAME"
+    echo "--------"
 
     # Print ffmpeg version to logs
     ffmpeg -version
 
-    # Print ffprobe to logs, check properties of initial video?
-    ffprobe -v error -show_streams $SAMPLE
+    # Print ffprobe to logs
+    # TODO: check/extract properties of initial video (8-bit vs 10-bit, rgb vs greyscale)
+    ffprobe -v error -show_streams $INPUT_VIDEO
 
-    # Run ffmpeg with the corresponding crf value
-    # - y: Overwrite output files without asking.
-    # - c:v codec for the video stream (decoder if used in front of input, encoder if used in front of output).
-    # - libx264: Sets the video compression to use H264. If RGB, use libx264rgb
-    # - preset superfast: Sets a number of parameters that enable reliable seeking
-    # - c:a # keep audio as is if present
-    ffmpeg -y -i "$SAMPLE" \
+    # Run compression with the corresponding CRF value
+    # - y: overwrite output files without asking.
+    # - c:v: specify codec for the video stream
+    #   (decoder if used in front of input, encoder if used in front of output).
+    # - libx264: sets the video compression to use H264. If RGB, use libx264rgb
+    # - preset superfast: sets a number of parameters that pressumably enable reliable seeking
+    # - c:a # keep audio stream as is if present
+    ffmpeg -y -i "$INPUT_VIDEO" \
     -c:v libx264 \
     -pix_fmt yuv420p \
     -preset superfast \
     -crf "${CRF_VALUES[${SLURM_ARRAY_TASK_ID}]}" \
     -c:a copy \
-    $REENCODED_VIDEO_PATH_MP4
+    "$REENCODED_VIDEO_PATH_MP4"
+
+    # collect status of previous command
+    status_ffmpeg_compress=$?
 
 
-    # print only if previous command is successful!
-    echo "Reencoded video: $REENCODED_VIDEO_PATH_MP4"
-    echo "--------"
+    # print only if previous command is successful
+    if [[ "$status_ffmpeg_compress" -eq 0 ]] ; then
+        echo "Reencoded video: $REENCODED_VIDEO_PATH_MP4"
+        echo "--------"
+    else
+        echo "ERROR reencoding video: $REENCODED_VIDEO_PATH_MP4"
+        echo "--------"
+    fi
 
-
-    # Reencoded videos log
-    # copy .err file to go with reencoded video too if required
-    # filename: {reencoded video name}.{slurm_array}.{slurm_job_id}
-    # TODO: make a nicer log
-    for ext in err out
-    do
-    cp slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.$ext \
-    /$REENCODED_VIDEOS_DIR/"$filename_out_no_ext".slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.$ext
-    done
-
-    # Frame extraction logs
-    # Move logs for this job to subdir with extracted frames
-    mv slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.{err,out} /$LOG_DIR
+    # Move logs across
+    mv slurm_array.$SLURMD_NODENAME.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.{err,out} \
+    /$LOG_DIR/"$filename_out_no_ext".slurm_array.$SLURM_ARRAY_JOB_ID-$SLURM_ARRAY_TASK_ID.{err,out}
 
 done
