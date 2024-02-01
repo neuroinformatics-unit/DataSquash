d4c743db83b27a4ce58d49bd26cf118e17087f7c
sfmig <33267254+sfmig@users.noreply.github.com>
2024-02-01 14:00:13 +0000

    simplify tests

--- tests/test_unit/test_video_compression.py
+++ tests/test_unit/test_video_compression.py
@@ -1,61 +1,72 @@
 from pathlib import Path
 
-import sleap
+from sleap.io.dataset import Labels
 
 from datasquash.video_compression.generate_label_files import (
     overwrite_video_in_slp_file,
 )
 
 
-def test_overwrite_video_in_slp_file(tmp_path: Path):
-    # input data
+def test_overwrite_video_in_slp_file(
+    tmp_path: Path,
+):
+    """Check if we overwrite correctly the video file in an slp file.
+
+    Parameters
+    ----------
+    tmp_path : Path
+        Pytest fixture providing a temporary path
+    """
+    # input reference labels file
     input_labels_path = str(
         Path(__file__).parents[1] / "data" / "courtship_labels.slp",
     )
-    new_video_path = "TEST_VIDEO.mp4"
+
+    # video to overwrite with
+    new_video_filename = "TEST_VIDEO.mp4"
+
+    # output labels file
     output_labels_path = str(tmp_path / "courtship_labels_OUT.slp")
 
-    # overwrite video in labels file
+    # overwrite video
     overwrite_video_in_slp_file(
         input_labels_path,
-        new_video_path,
+        new_video_filename,
         output_labels_path,
     )
 
-    # check output labels file
-    original_labels = sleap.Labels.load_file(str(input_labels_path))
-    updated_labels = sleap.Labels.load_file(str(output_labels_path))
-
-    # read sleap video from path for comparison
-    sleap_video_from_file = sleap.Video.from_filename(new_video_path)
-    sleap_video_from_file.backend._detect_grayscale = False
-
-    # assert labelled frames are the same
-    for lf_original, lf_update in zip(
-        original_labels.labeled_frames,
-        updated_labels.labeled_frames,
+    # inspect output labels file
+    labels_dict = {}
+    for labels_str, labels_path in zip(
+        ["old", "new"],
+        [input_labels_path, output_labels_path],
     ):
-        # assert video backend
-        assert all(
-            getattr(lf_update.video.backend, x)
-            == getattr(sleap_video_from_file.backend, x)
-            for x in sleap_video_from_file.backend.__dict__
+        labels_dict[labels_str] = Labels.load_file(
+            labels_path,
+            video_search=Labels.make_video_callback(
+                [str(Path(labels_path).parent)],
+            ),
         )
 
-        # assert frame idx
-        assert lf_original.frame_idx == lf_update.frame_idx
+    # check one video in new labels
+    assert len(labels_dict["new"].videos) == 1
 
-        # assert labelled instances (they include links to the video)
-        for ins_original, ins_update in zip(
-            lf_original.instances,
-            lf_update.instances,
+    # check video is set to the desired value
+    assert labels_dict["new"].videos[0].filename == new_video_filename
+
+    # check same number of labelled frames as before
+    list_lfs_new = labels_dict["new"].find(labels_dict["new"].videos[0])
+    list_lfs_old = labels_dict["old"].find(labels_dict["old"].videos[0])
+    assert len(list_lfs_new) == len(list_lfs_old)
+
+    # check annotations at each labelled frame
+    for lf_new, lf_old in zip(list_lfs_new, list_lfs_old):
+        for annot_new, annot_old in zip(
+            lf_new.instances,
+            lf_old.instances,
         ):
-            assert all(
-                getattr(ins_update.video.backend, x)
-                == getattr(sleap_video_from_file.backend, x)
-                for x in sleap_video_from_file.backend.__dict__
-            )
-
-            assert ins_original.frame_idx == ins_update.frame_idx
-            assert ins_original.points == ins_update.points
-            assert ins_original.track == ins_update.track
+            # check frame_idx, points, track are the same
+            assert annot_new.video.filename == new_video_filename
+            assert annot_new.frame_idx == annot_old.frame_idx
+            assert annot_new.points == annot_old.points
+            assert annot_new.track == annot_old.track
